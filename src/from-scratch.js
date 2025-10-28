733834207fff02801f617de2a7b9e2c4a56a5000
Marcy Student <Marcy_Student@MARCY-MBAir40.local>
2025-10-28 15:25:07 -0400

    solved problems

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,11 +1,34 @@
 // Create callback functions for Higher-Order Functions:
-const logEachValue = () => { };
 
-const makePeopleHappy = () => { };
+const logEachValue = (arr, callback) => {
+  return arr.forEach(word => callback(word))
+};
+const eachValue = logEachValue (["a", "b", "c"], item => item)
+console.log(eachValue)
 
-const getEvenNumbers = () => { };
 
-const doubleEveryNumber = () => { };
+const makePeopleHappy = (arrPeople, callback) => {
+   return arrPeople.forEach(word => callback(word))
+ };
+ people.isHappy = people.isHappy = true
+ console.log(people.isHappy)
+
+
+const getEvenNumbers = (evenArr, callback) => { 
+    return evenArr.filter(num => callback(num))
+};
+  const isEven = num => num % 2 === 0;
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+
+
+const doubleEveryNumber = (arrNums, callback) => { 
+  return arrNums.map(num => callback(num))
+};
+  const result = doubleEveryNumber([1, 2, 3], item => item * 2 );
+  console.log(result);
+
+
 
 const convertToBooleans = () => { };
 
@@ -30,3 +53,6 @@ module.exports = {
   myFind,
   myFilter,
 };
+
+
+
