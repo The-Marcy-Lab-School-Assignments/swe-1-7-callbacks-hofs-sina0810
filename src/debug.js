68b2fdd076a390e662fc98261ee28e1a8ddf4ab2
Marcy Student <Marcy_Student@mac.mynetworksettings.com>
2025-10-28 23:14:35 -0400

    problem solved

--- src/debug.js
+++ src/debug.js
@@ -1,17 +1,20 @@
 const myEvery = (arr, callback) => {
   for (const value of arr) {
-    if (callback) return false;
+    if (callback(value) ){
+        return ture
+  } else {
+    return false
   }
-  return true;
+}
 };
 
 const sortUsersBy = (users, sortingCallback) => {
   return [...users].sort(sortingCallback());
 };
 
-const logEachName = (names) => {
-  return names.forEach(console.log());
-};
+const logEachName = (names, i, arr) => {
+   names.forEach(
+    console.log(names,i,arr))
 
 const logEachUserBio = (users) => {
   return users.forEach(console.log(users.bio));
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,43 +1,57 @@
 // Create callback functions for Higher-Order Functions:
 
-const logEachValue = (arr, callback) => {
-  return arr.forEach(word => callback(word))
+const logEachValue = (arr) => {
+   arr.forEach((value , index) => {
+    console.log(`Value: ${value}, index: ${index}.`)
+   })
 };
-const eachValue = logEachValue (["a", "b", "c"], item => item)
-console.log(eachValue)
 
 
-const makePeopleHappy = (arrPeople, callback) => {
-   return arrPeople.forEach(word => callback(word))
- };
- people.isHappy = people.isHappy = true
- console.log(people.isHappy)
-
-
-const getEvenNumbers = (evenArr, callback) => { 
-    return evenArr.filter(num => callback(num))
+const makePeopleHappy = (arr) => {
+   arr.forEach((people) => people.isHappy = true)
 };
-  const isEven = num => num % 2 === 0;
-console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]))
+   
 
 
+const getEvenNumbers = (arrNum) => {
+ return arrNum.filter((n) => n % 2 === 0)
 
-const doubleEveryNumber = (arrNums, callback) => { 
-  return arrNums.map(num => callback(num))
 };
-  const result = doubleEveryNumber([1, 2, 3], item => item * 2 );
-  console.log(result);
 
 
 
-const convertToBooleans = () => { };
+const doubleEveryNumber = (arrNums) => {
+  return arrNums.map((nums) => nums * 2)
+};
+
+
+
+const convertToBooleans = (arr) => {   
+return arr.map((words) => Boolean(words))
+
+};
 
 // Create your own Higher Order Functions:
-const myForEach = () => { };
+const myForEach = (arr, callback) => {
+  arr.forEach((words) => callback(words))
+ };
+ const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie'];
+const result = myForEach(myNames, (name) => {
+  console.log(`Hi, ${name}!`)
+});
 
-const myMap = () => { };
 
-const myFilter = () => { };
+const myMap = (arr, transform) => {
+    const myNums = [];
+    arr.forEach((words => transform(words)))
+    words.push(myNums)
+ };
+
+
+const myFilter = (arr, test) => {
+  
+
+ };
 
 const myFind = () => { };
 
